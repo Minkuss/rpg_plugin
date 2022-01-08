21341e79af66a90c5e5740cc621a3f49e600d92b
tol4ok <73625734+tol4ok@users.noreply.github.com>
2022-01-08 17:36:07 +1200

    eblan ya

--- src/main/java/me/minkuss/rpg_plugin/listeners/PlayerBrakeBlockListener.java
+++ src/main/java/me/minkuss/rpg_plugin/listeners/PlayerBrakeBlockListener.java
@@ -8,8 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.List;
-
 public class PlayerBrakeBlockListener implements Listener {
     private final Rpg_plugin _plugin;
 
@@ -23,9 +21,8 @@ public class PlayerBrakeBlockListener implements Listener {
 
     private int GetBlockExp(Material block) {
         FileConfiguration config = _plugin.getConfig();
-        List<String> ores_exp = config.getStringList("exp-info.ores");
 
-        if(ores_exp.contains(block.toString())) {
+        if(config.contains("exp-info.ores." + block.toString())) {
             return config.getInt("exp-info.ores." + block);
         }
 
